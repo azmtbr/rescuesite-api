255973138e297936ac2712985d4a99917b903951
Jon Malis <jmalis@live.com>
2016-03-24 11:16:31 -0700

    able to remove gallery image

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module RescuesiteApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options, :delete]
       end
     end
   end
