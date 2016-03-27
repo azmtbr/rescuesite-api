c08eaa153541c6d98e61b2db526b373dc7a235d7
Jon Malis <jmalis@live.com>
2016-03-26 17:04:44 -0700

    added email format validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
 
+  has_secure_password
+
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
   validates :email,
             presence: true,
             uniqueness: { case_sensetive: false },
-            length: { minimum: 3, maximum: 254 }
+            length: { minimum: 3, maximum: 254 },
+            format: EMAIL_REGEX
 
-  has_secure_password
 end
