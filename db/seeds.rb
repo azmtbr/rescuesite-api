620b48c46a409280bc1cb238f97d2d25e7b92e3c
Jon Malis <jmalis@live.com>
2016-04-05 16:19:26 -0700

    change date format in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ Rescue.create! name: "Fearless", email:"fearless@email.com", landing_gallery: La
 Rescue.create! name: "Foothills Animal", email:"foothillsanimalrescue@email.com", landing_gallery: LandingGallery.new
 User.create! email: "jon@email.com", password: "password", password_confirmation: "password", rescue_id: 1
 User.create! email: "me@example.org", password: "password", password_confirmation: "password", rescue_id: 1
-Animal.create! name: "Koni", rescue_id: 1, sex: 'female', dob: 11/04/1967, breed: 'dog', gallery: Gallery.new
-Animal.create! name: "Molly", rescue_id: 1, sex: "female", dob: 11/04/1967, breed: "dog", gallery: Gallery.new
-Animal.create! name: "Aspen", rescue_id: 1, sex: 'male', dob: 11/04/1967, breed: 'dog', gallery: Gallery.new
+Animal.create! name: "Koni", rescue_id: 1, sex: 'female', dob: DateTime.now, breed: 'dog', gallery: Gallery.new
+Animal.create! name: "Molly", rescue_id: 1, sex: "female", dob: DateTime.now, breed: "dog", gallery: Gallery.new
+Animal.create! name: "Aspen", rescue_id: 1, sex: 'male', dob: DateTime.now, breed: 'dog', gallery: Gallery.new
 Adoption.create! animal: Animal.first, rescue_id: 1, first_name: "Matt", last_name: "Thompson", email: "matt@example.org"
 Adoption.create! animal: Animal.last, rescue_id: 1, first_name: "Jon", last_name: "Clarkson", email: "jon@example.org"
 # Gallery.create! animal_id: 2, id: 4
