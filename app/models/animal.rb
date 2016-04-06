555ee4f410be8c3a2216a092526eb5aea7ef6659
Jon Malis <jmalis@live.com>
2016-04-06 10:56:06 -0700

    testing path change for gallery image

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,7 +5,9 @@ class Animal < ActiveRecord::Base
   validates_presence_of :rescue_id, :name
 
   # Paperclip
-  has_attached_file :image, styles: { medium: "300x300>", large: "600x600>", thumb: "100x100>" }, default_url: ->(attachment) { ActionController::Base.helpers.asset_path('missing.png') }
+  has_attached_file :image,
+                    styles: { medium: "300x300>", large: "600x600>", thumb: "100x100>" },
+                    default_url: ->(attachment) { ActionController::Base.helpers.asset_path('missing.png') }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/, /JPE?G\Z/]
 
