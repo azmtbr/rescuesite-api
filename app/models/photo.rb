bcbe3b2a0126f0fe0937f5a3561656d4d6b0a825
Jon Malis <jmalis@live.com>
2016-04-06 16:54:16 -0700

    left a comma in

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,8 +5,7 @@ class Photo < ActiveRecord::Base
   # Paperclip
   has_attached_file :gallery_image,
                     styles: { small: "300x300>", medium: "600x600>", large: "900x900>" },
-                    default_url: ->(attachment) { ActionController::Base.helpers.asset_path('missing.png') },
-                    # path: '/animals/:attachment/:id/:style/:filename'
+                    default_url: ->(attachment) { ActionController::Base.helpers.asset_path('missing.png') }
   validates_attachment_content_type :gallery_image, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :gallery_image, matches: [/png\Z/, /jpe?g\Z/, /JPE?G\Z/]
 
